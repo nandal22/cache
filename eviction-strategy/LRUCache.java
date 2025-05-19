93dbdbf8a0d3597fa954569d5c9f455ed540acc3
nandal22 <sachinnandal87169@gmail.com>
2025-05-19 21:13:30 +0530

    LRU Cache Implementation

--- eviction-strategy/LRUCache.java
+++ eviction-strategy/LRUCache.java
@@ -1,3 +1,93 @@
-public static void main(String[] args){
-	System.out.println("LRU Cache");
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+class LRUCache{
+	public static void main(String[] args){
+		System.out.println("This is LRU Cache");
+
+		/*
+		 * What a cache contains
+		 * 1. Get Ops
+		 * 2. Put Ops
+		 * 3. Eviction Strategy - Remove Least Recently Used (LRU)
+		 * 4. Supports different Data Types
+		 */
+
+		 /*
+		  * Example:
+			Size : 5
+			1, 2, 3, 4, 5, 4, 3, 3, 2
+		  */
+
+		LRUCacheImpl<Integer, String> lruCache = new LRUCacheImpl<Integer, String>(5);
+
+		for(int i = 0; i < 10000; i++){
+			lruCache.put(i, "Value " + i);
+		}
+
+		System.out.println(lruCache.get(0));
+		System.out.println(lruCache.get(9999));
+
+		// Test 9: Repeated gets don't affect order if no puts
+		lruCache.put(1, "A");
+		lruCache.put(2, "B");
+
+		System.out.println(lruCache.get(1));
+		System.out.println(lruCache.get(1));
+		System.out.println(lruCache.get(1));
+
+		lruCache.put(3, "C"); 
+
+		System.out.println(lruCache.get(2));
+	}
+}
+
+interface IMyCache<K, V>{
+
+	public V get(K k);
+
+	public void put(K k, V v);
+}
+
+class LRUCacheImpl<K, V> implements IMyCache<K, V>{
+
+	int capacity;
+	Map<K, V> cache;
+	List<K> cacheKeys;
+
+	LRUCacheImpl(int capacity){
+		this.cache = new HashMap<K, V>();
+		this.capacity = capacity;
+		this.cacheKeys = new LinkedList<K>();
+	}
+
+	@Override
+	public V get(K key){
+		if(!cache.containsKey(key)){
+			return null;
+		}
+
+		cacheKeys.remove(key);
+		cacheKeys.addFirst(key);
+
+		return cache.get(key);
+	}
+
+	@Override
+	public void put(K key, V val){
+
+		if(cache.containsKey(key)){
+			cacheKeys.remove(key);
+		} else if(cache.size() >=capacity){
+			K lastKey = cacheKeys.getLast();
+			cache.remove(lastKey);
+		}
+
+		cacheKeys.addFirst(key);
+		cache.put(key, val);
+
+		return;
+	}
 }
